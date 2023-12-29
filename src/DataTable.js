b717f733265ad81ff329c32511d8c36e32085b29
Deepti <134826480+deepti-wali@users.noreply.github.com>
2023-12-29 10:24:43 +0530

    first commit

---
+++ src/DataTable.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import App from "./App";
+import Pagination from "./Pagination";
+const DataTable = ({ currentItems }) => {
+  return (
+    <table className="center">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Role</th>
+        </tr>
+      </thead>
+      <tbody>
+        {currentItems.map((item) => (
+          <tr key={item.id}>
+            <td>{item.id}</td>
+            <td>{item.name}</td>
+            <td>{item.email}</td>
+            <td>{item.role}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+export default DataTable;
